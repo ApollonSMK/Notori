e056db1908fe10670f97e92a8ca32e7b232740e5
Pablo Silva <microgramxx@gmail.com>
2025-08-20 01:46:49 +0000

    [block:9022206 txIndex:12]from: 0xe60...65e9cto: WZT.transfer(address,ui

--- src/contracts/WZT.sol
+++ src/contracts/WZT.sol
@@ -1,24 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
- * @title WZT Token
- * @dev A simple ERC20 test token with a minting function restricted to the owner.
+ * @title WZTToken
+ * @dev A simple ERC20 token for testing purposes in the NotoriStake Mini App.
+ * The owner can mint new tokens.
  */
 contract WZT is ERC20, Ownable {
-    constructor() ERC20("Wize Test Token", "WZT") {
-        // Mint an initial supply of 1,000,000 tokens to the deployer.
-        _mint(msg.sender, 1_000_000 * 10 ** decimals());
+    /**
+     * @dev Constructor that sets the token name and symbol, and mints an initial supply to the contract deployer.
+     */
+    constructor() ERC20("Wize Token", "WZT") Ownable(msg.sender) {
+        _mint(msg.sender, 1000000 * (10 ** decimals()));
     }
 
     /**
-     * @dev Creates `amount` new tokens and assigns them to `to`.
-     * Can only be called by the owner.
+     * @dev Creates `amount` new tokens and assigns them to `to`, increasing the total supply.
+     * Only the owner of the contract can call this function.
      * @param to The address that will receive the minted tokens.
-     * @param amount The a-mount of tokens to mint.
+     * @param amount The amount of tokens to mint.
      */
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
